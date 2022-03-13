3a03fb8f43a6b51c1ba96bdc611c3f0afb5ff8ec
Jack Barry <Jack.Barry@FlashParking.com>
2022-03-13 12:32:25 -0600

    experiment: ⚗  Try some different graph props

--- helpers/makeMetaTags.ts
+++ helpers/makeMetaTags.ts
@@ -10,44 +10,49 @@ export default function (
 
   return [
     {
+      property: 'og:type',
+      content: 'website'
+    },
+    { property: 'og:url', content: 'https://devdad.life' },
+    {
       property: 'og:image',
       name: 'image',
       content: imageUrl
     },
-    {
-      hid: 'og:description',
-      name: 'description',
-      content: description
-    },
-    {
-      name: 'author',
-      content: 'Jack Barry'
-    },
-    // Facebook
+    // {
+    //   hid: 'og:description',
+    //   name: 'description',
+    //   content: description
+    // },
+    // {
+    //   name: 'author',
+    //   content: 'Jack Barry'
+    // },
+    // // Facebook
     {
       property: 'og:title',
       content: title
-    },
-    {
-      property: 'og:site_name',
-      content: 'DevDad.life'
-    },
-    // Twitter
-    {
-      name: 'twitter:card',
-      content: 'summary_large_image'
-    },
-    {
-      property: 'twitter:title',
-      content: title
-    },
-    {
-      property: 'twitter:description',
-      content: description
-    },
-    {
-      name: 'twitter:image',
-      content: imageUrl
     }
+    // {
+    //   property: 'og:site_name',
+    //   content: 'DevDad.life'
+    // },
+    // Twitter
+    // {
+    //   name: 'twitter:card',
+    //   content: 'summary_large_image'
+    // },
+    // {
+    //   property: 'twitter:title',
+    //   content: title
+    // },
+    // {
+    //   property: 'twitter:description',
+    //   content: description
+    // },
+    // {
+    //   name: 'twitter:image',
+    //   content: imageUrl
+    // }
   ]
 }
